0c9351f5d9870fc39fc9d7836cda924e0fe36b13
shakey200592 <shakey1992jm@hotmail.co.uk>
2024-06-08 00:55:38 +0100

    feat: add class for TextNode and initial propertoes and methods

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from textnode import TextNode
+
+def main():
+    node = TextNode("hello world", "normal", "http://www.helloworld.co.uk")
+    print(node)
+    
+    
+    
+    
+    
+    
+    
+    
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,2 +1,16 @@
-class TextNode(text,text_type,url):
-    pass
+class TextNode():
+    
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+        
+    def __eq__(self, other_text_node):
+        if self.text == other_text_node.text and self.text_type == other_text_node.text_type and self.url == other_text_node.url:
+            return True
+        else:
+            return False
+        
+    def __repr__(self):
+        return f"(TextNode:({self.text}, {self.text_type}, {self.url}))"
+    
